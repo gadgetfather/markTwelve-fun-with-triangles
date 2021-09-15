b8be90d6ca1d7a315c199fb0c164d8e455bf7107
gadgetfather <kamblea635@gmail.com>
2021-09-15 11:00:24 +0530

    Update app.js
    added istriangle checkmark

--- app.js
+++ app.js
@@ -8,9 +8,22 @@ function sumOfAngle(angle1,angle2,angle3){
     const sum = angle1 + angle2 + angle3;
    return sum;
 }
+function outputMessage(message){
+    outputEl.textContent= message
+}
 
 function isTriangle(){
-    sumOfAngle(45,45,45)
+    let a1 = Number(inputs[0].value)
+    let a2 = Number(inputs[1].value)
+    let a3 = Number(inputs[2].value)
+    const sumOfAngles = sumOfAngle(a1,a2,a3)
+    console.log(sumOfAngles)
+    if(sumOfAngles===180){
+        outputMessage("It is a triangle")
+    }
+    else{
+        outputMessage("it is not triangle")
+    }
     
 
 }
