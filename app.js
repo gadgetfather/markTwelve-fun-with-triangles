e6f0a1040741b83de4c20682fd2a1ccbf85c3b42
gadgetfather <kamblea635@gmail.com>
2021-09-15 10:32:06 +0530

    created sumofangles

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 const inputs = document.querySelectorAll(".angle-input")
 const isTriangleBtn = document.querySelector("#is-triangle")
 const outputEl = document.querySelector("#output")
+
+isTriangleBtn.addEventListener('click',isTriangle)
+
+function sumOfAngle(angle1,angle2,angle3){
+    const sum = angle1 + angle2 + angle3;
+   return sum;
+}
+
+function isTriangle(){
+    sumOfAngle(45,45,45)
+    
+
+}
